d831e1cae596a510f5df55b3527b125ad70ce425
Default <163393531+Vadimbgs@users.noreply.github.com>
2025-09-28 18:06:59 +0300

    Add files via upload

---
+++ js_3/js_3.Домашка/js/script.js
@@ -0,0 +1,49 @@
+// создаём переменную для хранения цвета глаз
+// let userEyeColor;
+
+// userEyeColor = 'белый';
+
+// console.log(userEyeColor);
+
+// userEyeColor = 'розовый';
+
+// console.log(userEyeColor);
+
+
+// let userName;
+// let user;
+
+// userName = 'Вася';
+
+// user = userName;
+
+// console.log(user);
+
+/*
+
+const BLOCK_SIZE = 14 + 50;
+
+console.log(BLOCK_SIZE);
+
+*/
+
+/*
+
+const BLOCK_HEIGHT = 100;
+
+console.log(BLOCK_HEIGHT);
+
+*/
+
+
+/*
+
+if (true) {
+    var size = 15;
+}
+
+console.log(size);
+
+*/
+
+
