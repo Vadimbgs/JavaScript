2845a09ce8194c70deb0533731c03cd211e53961
Default <163393531+Vadimbgs@users.noreply.github.com>
2025-09-28 17:38:29 +0300

    Add files via upload

---
+++ js_3/js/script.js
@@ -0,0 +1,32 @@
+// Обьевление контейнеров
+
+// let myAge = 36;
+// let myNewAge = 18;
+
+// myAge = myNewAge;
+// console.log(myAge);
+
+
+
+
+//  Создание константы
+
+// const userProfile = {
+//     name: 'Фрилансер',
+//     age: 36,
+//     message: 'Живи, работай и розвивайся в свободное время!'
+// }
+// console.log(userProfile);
+
+// userProfile.age = 18;
+
+// console.log(userProfile);
+
+
+// Обьевление var
+
+function testBlockVar() {
+    var myAgeVar = 36;
+}
+
+testBlockVar();
