b2a73b5a87f5b5fbb187adc08e588a7ecfff0654
Default <163393531+Vadimbgs@users.noreply.github.com>
2025-09-29 15:38:45 +0300

    Add files via upload

---
+++ js_4/js/script.js
@@ -0,0 +1,96 @@
+// let userName;
+
+// userName = "Фрилансер";
+
+// console.log(typeof userName);
+
+// console.log(userName);
+
+
+
+// if (userName === undefined) {
+//     console.log('Переменная НЕ определена');
+// } else {
+//     console.log('Переменная определена');
+// }
+
+
+// if (typeof userName === 'undefined') {
+//     console.log('Перменная НЕ определена');
+// } else {
+//     console.log('Переменная определена');
+// }
+
+
+
+// Переменная null
+
+// let userName = null;
+// console.log(userName);
+
+
+
+// let block = document.querySelector('.block');
+
+// console.log(block);
+
+// console.log(typeof block);
+
+//  Тип bool
+
+
+// let willYouMarryMe = true;
+// if (willYouMarryMe) {
+//     console.log(':)');
+// } else {
+//     console.log(':(');
+// }
+
+
+
+
+// let trueOrFalse = 58 < 18;
+// console.log(trueOrFalse);
+
+
+
+// let getInfinity = 58 / 0;
+// console.log(getInfinity);
+// console.log(typeof getInfinity);
+
+
+// let getNan = 'Фрилансер' / 10;
+// console.log(getNan);
+// console.log(typeof getNan);
+
+
+// Тип bigInteger
+
+
+// const bigInteger = 100000n;
+// console.log(typeof bigInteger);
+
+
+
+//String
+
+let userName = "Фрилансер по жизни";
+console.log(typeof userName);
+
+let userNameV2 = 'Фрилансер по жизни';
+console.log(typeof userNameV2);
+
+let userNameV3 = `Фрилансер по жизни`;
+console.log(typeof userNameV3);
+
+let userAge = 36;
+let userAgeInfo = `Возраст: ${userAge}`;
+console.log(userAgeInfo);
+
+
+
+// Обьекты
+
+// let userInfo = {
+
+// }
