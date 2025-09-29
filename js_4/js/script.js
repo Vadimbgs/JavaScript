e3d8f9e5932e98fb44f26a85c1eafd34ece22d92
Default <163393531+Vadimbgs@users.noreply.github.com>
2025-09-29 15:02:33 +0300

    Add files via upload

---
+++ js_4/js/script.js
@@ -0,0 +1,64 @@
+// let userName;
+
+// userName = "Фрилансер";
+
+// console.log(typeof userName);
+
+// console.log(userName);
+
+
+
+// if (userName === undefined) {
+//     console.log('Переменная НЕ определена');
+// } else {
+//     console.log('Переменная определена');
+// }
+
+
+// if (typeof userName === 'undefined') {
+//     console.log('Перменная НЕ определена');
+// } else {
+//     console.log('Переменная определена');
+// }
+
+
+
+// Переменная null
+
+// let userName = null;
+// console.log(userName);
+
+
+
+// let block = document.querySelector('.block');
+
+// console.log(block);
+
+// console.log(typeof block);
+
+//  Тип bool
+
+
+// let willYouMarryMe = true;
+// if (willYouMarryMe) {
+//     console.log(':)');
+// } else {
+//     console.log(':(');
+// }
+
+
+
+
+// let trueOrFalse = 58 < 18;
+// console.log(trueOrFalse);
+
+
+
+// let getInfinity = 58 / 0;
+// console.log(getInfinity);
+// console.log(typeof getInfinity);
+
+
+let getNan = 'Фрилансер' / 10;
+console.log(getNan);
+console.log(typeof getNan);
