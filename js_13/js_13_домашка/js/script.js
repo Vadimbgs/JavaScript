873f9e79121b0274c21da4b825f291650e20cf37
Default <163393531+Vadimbgs@users.noreply.github.com>
2025-10-17 17:35:51 +0300

    Add files via upload

---
+++ js_13/js_13_домашка/js/script.js
@@ -0,0 +1,53 @@
+//ДОМАШКА
+/*
+1. Изучить теорию
+2. Решить задачи:
+*/
+/*
+// Задача №1.
+// Получить в переменную элемент с атрибутом data-say-hi
+// и прочитать значение этого атрибута
+// <div data-say-hi="yes">Привет!</div>
+
+// Решение
+
+// const elemOne = document.querySelector('[data-say-hi]');
+// const attr = elemOne.getAttribute('data-say-hi');
+// console.log(attr);
+*/
+
+/*
+// Задача №2.
+// Получить в переменную элемент с текстом Йончи
+<ul>
+	<li>Корчи</li>
+	<li>Йончи</li>
+</ul>
+*/
+/*
+// Задача №3.
+// Получить в переменную коллекцию элементов с классом like
+<div class="like"></div>
+<div class="subscribe"></div>
+<div class="like subscribe"></div>
+*/
+/*
+// Задача №4.
+// Куда добавится <li>Текст</li> ?
+
+// JS
+const list = document.querySelectorAll('ul');
+list.insertAdjacentHTML(
+	'beforeend',
+	'<li>Текст</li>'
+);
+
+// HTML
+<ul>
+	<li>Пункт №1</li>
+	<li>Пункт №2</li>
+</ul>
+*/
+
+
+
